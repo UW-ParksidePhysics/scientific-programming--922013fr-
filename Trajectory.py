4e08a476fabc0ae38972b88b5db5a8d95ab68809
JRoszko <98778217+JRoszko@users.noreply.github.com>
2022-03-07 16:01:53 -0600

    Add files via upload

---
+++ Trajectory.py
@@ -0,0 +1,9 @@
+standard_gravity = 9.80665  # m/s/s
+initial_velocity = 15.  # km/h
+launch_angle = 60  # °
+horizontal_range = 0.5  # m
+initial_height = 1.0  # m
+
+print(f'v0 = {initial_velocity:.1f} km/h')
+print(f'theta = {launch_angle:.0f} °')
+print(f'x = {initial_velocity:.1f} km/h')
