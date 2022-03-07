7758a4ca96880094538e63c5cd4b9e2e4a5e3e8f
JRoszko <98778217+JRoszko@users.noreply.github.com>
2022-03-07 16:55:35 -0600

    Add files via upload

--- python/Trajectory.py
+++ python/Trajectory.py
@@ -6,16 +6,18 @@ initial_height = 1.0  # m
 
 print(f'v0 = {initial_velocity:.1f} km/h')
 print(f'theta = {launch_angle:.0f} °')
-print(f'x = {initial_velocity:.1f} km/h')
+print(f'x = {initial_velocity:.1f} m')
 print(f'y0 = {initial_height:.1f} m')
 
 from math import pi, tan, cos
 
-# Convert initial velocity to m/s 
+# Convert initial velocity to m/s
 initial_velocity = initial_velocity * 3600/1000
+# Convert angle to radians
 launch_angle = launch_angle * pi/180
 
-height = horizontal_range * tan(launch_angle) -\
-	1/(2*initial_velocity**2)*standard_gravity*horizontal_range**2/((cos(launch_angle))**2)\
-	+ initial_height
-print(f'y     ={height:.1f}m')
+height = horizontal_range * tan(launch_angle) \
+         - 1/(2*initial_velocity**2)*standard_gravity*horizontal_range**2/((cos(launch_angle))**2) \
+         + initial_height
+print(f'y     = {height:.1f} m')
+
