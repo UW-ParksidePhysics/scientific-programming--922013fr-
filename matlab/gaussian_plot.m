d095f08f4528608709e8a42c01092f19744936c5
922013fr <98779818+922013fr@users.noreply.github.com>
2022-03-29 12:22:53 -0500

    Update gaussian_plot.m

--- matlab/gaussian_plot.m
+++ matlab/gaussian_plot.m
@@ -1,16 +1,12 @@
-x = 0:10/100:10;
+x = 0:1/100:10;
 sigmas = 0.5:0.5:1.5;
 mu = 5;
 ys = [];
-for sigma = sigmas
-    y = (1/sqrt(2*pi*sigma^2))*exp(-(x-mu).^2/(2*sigma^2))
-    plot (x,y)
-    ys = [ys;y]
-    
-end 
-plot(x,ys)
+for sigma = sigmas 
+    y = (1/sqrt(2*pi*sigma^2))*exp(-(x-mu).^2/(2*sigma^2));
+    ys = [ys;y];
+end
+plot(x,ys(1,:),'r--',x,ys(2,:),'b-.',x,ys(3,:),'g')
 xlabel('x')
-ylabel'\phi(x-5),\sigma'
+ylabel('\phi(x-5,\sigma)')
 legend('0.5','1.0','1.5')
-line(x,ys'Color','red','Linestyle','- -')
-
