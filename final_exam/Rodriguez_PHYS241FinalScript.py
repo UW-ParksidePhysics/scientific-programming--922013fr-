d0260ee8706a757e07203501ba4f17be830f3fb0
922013fr <98779818+922013fr@users.noreply.github.com>
2022-05-09 15:59:34 -0500

    Update Rodriguez_PHYS241FinalScript.py

--- final_exam/Rodriguez_PHYS241FinalScript.py
+++ final_exam/Rodriguez_PHYS241FinalScript.py
@@ -7,3 +7,12 @@ from equations_of_state import fit_eos
 from convert_units import convert_units
 from numpy import linspace
 import matplotlib.pyplot as plt
+
+display_graph = True 
+
+def parse_file_name(file_name):
+  to_parse = file_name.split(".")
+  symbol = to_parse[0]
+  structure = to_parse[1]
+  acronym = to_parse[2]
+  return symbol, structure, acronym   #1
