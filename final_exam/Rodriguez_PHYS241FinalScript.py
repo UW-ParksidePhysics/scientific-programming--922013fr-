63d811f8fd17dfad0a4643c057964272cbe70103
franciscorodriguez <rodri222@rangers.uwp.edu>
2022-05-09 18:09:55 -0500

    final review

--- final_exam/Rodriguez_PHYS241FinalScript.py
+++ final_exam/Rodriguez_PHYS241FinalScript.py
@@ -1,10 +1,12 @@
-from bivariate_statistics import bivariate_statistics
-from equations_of_state import fit_eos
-from final_exam.convert_units import convert_units
-from final_exam.plot_data_with_fit import plot_data_with_fit
-from python.fit_curve_array import fit_curve_array
-from quadratic_fit import quadratic_fit
 from two_column_text_read import two_column_text_read
+from bivariate_statistics import bivariate_statistics
+from quadratic_fit import quadratic_fit
+from fit_curve_array import fit_curve_array
+from plot_data_with_fit import plot_data_with_fit
+from equations_of_state import fit_eos
+from convert_units import convert_units
+from numpy import linspace
+import matplotlib.pyplot as plt
 
 display_graph = True
 
@@ -29,7 +31,7 @@ max_x = statistics[3]
 
 undo_array = zip(*array)
 array_2 = list(undo_array)
-fit_eos_curve, fit_parameters = fit_eos(array[0], array[1], quadratic_coefficients, eos='birch-murnaghan',
+fit_eos_curve, fit_parameters = fit_eos(array[0], array[1], quadratic_coefficients, eos='birch-murrain',
                                         number_of_points=50)
 bulk_modulus = fit_parameters[1]
 equilibrium_volume = fit_parameters[3]
@@ -51,12 +53,13 @@ def annotate_graph(symbol, structure):
     plt.axvline(eq_vol - array_2[0][array_2[1].index(min(array_2[1]))] * 0.01, color="black", linestyle='-')
 
     plt.text(91, -0.0025, "created by Francisco Rodriguez May/09/2022")
-    plt.title("{} Equation of State for {} in DFT {}".format('birch-murnaghan', symbol, acronym))
+    plt.title("{} Equation of State for {} in DFT {}".format('birch-murrain', symbol, acronym))
     return ax, plt
 
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
+
 
 volumes = linspace(min(array_2[0]), max(array_2[0]), len(fit_eos_curve))
 line1, = ax.plot(array_2[0], array_2[1], 'o')
@@ -83,35 +86,3 @@ if display_graph:
 elif not display_graph:
     plt.savefig("Cu.Fm-3m.GGA-PBE.volumes_energies.dat")
 
-###########################################
-from generate_matrix import generate_matrix
-from lowest_eigenvectors import lowest_eigenvectors
-from numpy import linspace
-import matplotlib.pyplot as plt
-
-display_graph = False
-potential_name = 'harmonic'
-N_dim = 120
-potential_parameter = 100
-
-matrix = generate_matrix(-10, 10, N_dim, potential_name, potential_parameter)
-# print(matrix)
-eigenvalues, eigenvectors = lowest_eigenvectors(matrix, 3)
-
-x = linspace(-10, 10, N_dim)
-line1, = plt.plot(x, eigenvectors[0][0:N_dim])
-line2, = plt.plot(x, eigenvectors[1][0:N_dim])
-line3, = plt.plot(x, eigenvectors[2][0:N_dim])
-
-plt.xlabel("x [a.u.]")
-plt.ylabel("ψ n ( x ) [a.u.]")
-plt.legend((line1, line2, line3), ('ψ1, Ε1 = 0.62414396 a.u.', 'ψ2, Ε2 = 0.87335307 a.u.', 'ψ3, Ε3 = 1.12229893 a.u.'))
-plt.axis([-10, 10, max(eigenvectors[0]) - 2, max(eigenvectors[0]) + 2])
-plt.axhline(color="black")
-plt.text(-9.5, -1.75, "Created by Peter Kveton 2021/05/12")
-plt.title("Select Wavefunctions for a Harmonic Potential on a Spatial Grid of 0, 1, 2 Points")
-
-if display_graph:
-    plt.show()
-elif not display_graph:
-    plt.savefig("Kveton.harmonic.Eigenvector0, 1, 2.png")
