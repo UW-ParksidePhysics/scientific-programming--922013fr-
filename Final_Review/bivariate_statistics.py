4b7ff924cd83fd9a1a4d00a10086ad6301aa9c6b
922013fr <98779818+922013fr@users.noreply.github.com>
2022-05-09 14:52:59 -0500

    Update bivariate_statistics.py

--- Final_Review/bivariate_statistics.py
+++ Final_Review/bivariate_statistics.py
@@ -3,6 +3,8 @@ from scipy import stats
 
 
 def bivariate_statistics(data):
+    if len(data) != 2 or len(data[0]) <= 1:
+        raise IndexError("Inappropriate Dimensions")
     stat = stats.describe(data)
     mean_y = stat.minmax[0][1]
     x_min, x_max = stat.minmax[0][0], stat.minmax[1][0]
@@ -17,4 +19,4 @@ if __name__ == "__main__":
     y = [2, 4, 6]
     test_array = np.array([x, y])
     print(bivariate_statistics(test_array))
-    
+   
