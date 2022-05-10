12f2609267cb4be73a4a0fa587bf0c72ff2a6b78
franciscorodriguez <rodri222@rangers.uwp.edu>
2022-05-10 12:48:02 -0500

    final

--- final_exam/TEST.py
+++ final_exam/TEST.py
@@ -11,9 +11,6 @@ import matplotlib.pyplot as plt
 from numpy import linspace
 
 display_graph = True
-potential_name = 'square'
-N_dim = 130
-potential_parameter = 200
 
 
 def parse_file_name(file_name):
@@ -24,17 +21,17 @@ def parse_file_name(file_name):
     return symbol, structure, acronym
 
 
-file_name = "Cu.Fm-3m.GGA-PBE.volumes_energies.dat"
+file_name = "Ag.Fm-3m.GGA-PBE.volumes_energies.dat"
 symbol, structure, acronym = parse_file_name(file_name)
-array = two_column_text_read("Cu.Fm-3m.GGA-PBE.volumes_energies.dat")
-statistics = bivariate_statistics(array)
+array = two_column_text_read("Ag.Fm-3m.GGA-PBE.volumes_energies.dat")
 statistics = bivariate_statistics(array)
 quadratic_coefficients = quadratic_fit(array)
 
 min_x = statistics[2]
 max_x = statistics[3]
 
-fit_eos_curve, fit_parameters = fit_eos(array[0], array[1], quadratic_coefficients, eos='Birch-Murnaghan',
+
+fit_eos_curve, fit_parameters = fit_eos(array[0], array[1], quadratic_coefficients, eos='vinet',
                                         number_of_points=50)
 bulk_modulus = fit_parameters[1]
 equilibrium_volume = fit_parameters[3]
@@ -53,12 +50,14 @@ def annotate_graph(axes, symbol, structure, bulk_modulus, equilibrium_volume, da
     y_min = data_extremes[2] - y_range * 0.10
     y_max = data_extremes[3] + y_range * 0.10
 
+
+
     plt.xlim(x_min, x_max)
     plt.ylim(y_min, y_max)
     # y_formatter = ScalarFormatter(useOffset=False)
     # axes.yaxis.set_major_formatter(y_formatter)
 
-    axes.annotate(symbol, xy=(x_min + 0.03 * x_range, y_max - 0.05 * y_range))
+    axes.annotate(symbol, xy=(x_min+0.03*x_range, y_max-0.05*y_range))
 
     axes.annotate(r'$ {}\overline{{{}}} {}$'.format(structure[0:2],
                                                     structure[3],
@@ -66,17 +65,17 @@ def annotate_graph(axes, symbol, structure, bulk_modulus, equilibrium_volume, da
                   xy=(115, 0))
 
     axes.annotate(r'$K_0$={:.2f} GPa'.format(bulk_modulus_gpa),
-                  xy=(0.5 * x_range + x_min, 0.5 * y_range + y_min))
+                  xy=(0.5*x_range+x_min, 0.5*y_range+y_min))
 
     axes.annotate(r'$V_0$={:.3f}Å$^3$/atom'.format(equilibrium_volume),
                   xy=(115, -0.001))
 
     graph_y_range = y_max - y_min
-    vertical_line_y_max = 1.3 * (data_extremes[2] - y_min) / graph_y_range
+    vertical_line_y_max = 1.3*(data_extremes[2] - y_min) / graph_y_range
     plt.axvline(x=equilibrium_volume, ymax=vertical_line_y_max, color="black", linestyle='--')
 
-    plt.text(91, -0.0025, "Created by Francisco Rodriguez 2022-05-09")
-    plt.title("{} Equation of State for {} in DFT {}".format('Birch-Murnaghan', symbol, acronym))
+    plt.text(91, -0.0025, "Created by Jordan Roszko 2022-05-09")
+    plt.title("{} Equation of State for {} in DFT {}".format('Vinet', symbol, acronym))
     plt.xlabel(r'$V$ (Å$^3$/atom)')
     plt.ylabel(r'$E$ (eV/atom)')
     return axes
@@ -84,9 +83,12 @@ def annotate_graph(axes, symbol, structure, bulk_modulus, equilibrium_volume, da
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
+#
+#
 # volumes = linspace(min_x, max_x, len(fit_eos_curve))
 # line1, = ax.plot(array_2[0], array_2[1], 'o')
 # line2, = ax.plot(volumes, fit_eos_curve, color="black")
+#
 
 
 annotate_graph(ax, symbol, structure, bulk_modulus, equilibrium_volume, statistics[2:])
